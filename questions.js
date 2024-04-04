02dc3e47646b1a69bed8a91cd7face4267a83826
Ryan Monteleone <ryan.monteleone@colorado.edu>
2024-04-04 09:42:31 -0600

    questions added

--- questions.js
+++ questions.js
@@ -1,8 +1,33 @@
 var questions = 
 [
     {
-        question: "What is 1+1",
-        options: ["1", "2", "3"],
-        answer:"2"
+        question: "Which type of sattelite communication technology is commonly used for internet access in remote areas?",
+        options: [
+            "VSAT (Very Small Aperture Terminal)", 
+            "DTH (Direct-to-Home)", 
+            "GPS (Global Positioning System)",
+            "GSM (Global System for Mobile Communication)",
+        ],
+        answer:"VSAT (Very Small Aperture Terminal)"
+    },
+    {
+        question: "What is the name of the international treaty that governs the use of space?",
+        options: [
+            "Geneva Convention", 
+            "Moon Treaty", 
+            "Kyoto Protocol",
+            "Outer Space Treaty",
+        ],
+        answer:"Outer Space Treaty"
+    },
+    {
+        question: "What is the name of the orbit used by most communication sattelite, where the sattelite's orbit matches the rotation of the Earth?",
+        options: [
+            "Loopus orbit", 
+            "Circular orbit", 
+            "Geostationary orbit",
+            "Low Earth orbit",
+        ],
+        answer:"Geostationary orbit"
     }
 ]
